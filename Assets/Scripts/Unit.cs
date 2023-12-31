dba971b29128b02b3f08bae507e70627e518b97b
Amirmohamad A <amirmohamada017@gmail.com>
2023-12-31 09:35:29 +0330

    Add unit movement animation.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,19 +1,30 @@
-using System;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 _targetPosition;
+    private const string IsWalking = "IsWalking";
+    private bool _isWalking;
 
     private void Update()
     {
         const float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, _targetPosition) > stoppingDistance)
         {
-            var moveDirection = (_targetPosition - transform.position).normalized; 
             const float moveSpeed = 4f; 
+            var moveDirection = (_targetPosition - transform.position).normalized;
             transform.position += moveDirection * (moveSpeed * Time.deltaTime);
-        }
+
+            const float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime*rotateSpeed);
+            
+            _isWalking = true;
+        } 
+        else
+            _isWalking = false;
+        
+        unitAnimator.SetBool(IsWalking, _isWalking);
 
         if (Input.GetMouseButtonDown(0))
             Move(MouseWorld.GetPosition());
